0750efa7f68d183e356d46de93e3fdf3bdef71c1
eungyukm <86348187+eungyukm@users.noreply.github.com>
2022-04-30 17:49:50 +0900

    Add files via upload

---
+++ EventChannel/EventChannel/Scripts/BaseClasses/DescriptionBaseSO.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This is ScriptableObject BaseSO
+/// </summary>
+public class DescriptionBaseSO : ScriptableObject
+{
+    [TextArea] public string description;
+}
